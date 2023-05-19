0b17e206475a9811c5d7bb3ac55a89494e7387e6
florin niculescu <florin.niculescu999@gmail.com>
2023-05-19 17:55:03 -0400

    models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake,CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name','desc']
+    inlines = [CarModelInline]
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,33 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    NAME = 'Name'
+    DESC = 'Description'
+    def __str__(self):
+        return "Name: " + self.NAME + ", " + \
+                "Description: " + self.DESC
 
-
+class CarModel(models.Model):
+    Carmake = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    Dealer_id = models.IntegerField(null=False, max_length=2, default=0)
+    Name = 'Name'
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    Type = models.CharField(
+        null=False,
+        max_length=10,
+        choices=[(SEDAN,'Sedan'),(SUV,'SUV'),(WAGON, 'Wagon')],
+        default=SEDAN
+    )
+    Year = models.DateField(null=True)
+    def __str__(self):
+        return "Car Make: " + self.Carmake + ", " + \
+                "Dealer_id: " + self.Dealer_id + ", " \
+                "Name: " + str(self.Name) + ", " + \
+                "Type: " + self.Type + ", " + \
+                "Year: " + self.str(self.Year)
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
