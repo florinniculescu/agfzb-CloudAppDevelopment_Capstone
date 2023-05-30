979e546eadcb1d99067822c154f750ed5ed1d118
florin niculescu <florin.niculescu999@gmail.com>
2023-05-30 15:20:11 -0400

    proxy update

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 # for static pages
 from django.views.generic import TemplateView
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -86,9 +86,14 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/3c122bad-f41e-4e17-bcbd-be4f913886e7/dealerships/get_all_dealerships"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 def get_dealership_list(request):
     context = {}
